e49cf323d618f4026ba001b8dba8d56054065ac9
Mariano Benitez <benitezmarianito@gmail.com>
2023-03-23 11:01:03 -0300

    Animaciones hero con css, animaciones en content client:idle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,19 @@ module.exports = {
         maps: {
           '0%, 100%': { opacity: '0' },
           '50%': { opacity: '100' },
-        }
+        },
+        "opacity01": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        "translatex150": {
+          "0%": { transform: "translateX(150px)" },
+          "100%": { transform: "translateX(0px)" },
+        },
+        "translatex150andopacity01": {
+          "0%": { transform: "translateX(-100px)", opacity: "0" },
+          "100%": { transform: "translateX(0px)", opacity: "1" },
+        },
       },
       animation: {
         maps: 'maps 3s ease-in-out infinite',
