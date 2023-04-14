347441b64705d03e28db0c2af2e9633bb5bb93c5
Mariano Benitez <benitezmarianito@gmail.com>
2023-04-14 09:51:22 -0300

    Correccion logo, mapa segun vh, nueva animacion

--- src/components/content/LogoAnimado.tsx
+++ src/components/content/LogoAnimado.tsx
@@ -5,13 +5,13 @@ const LogoAnimado = ({ children }: { children: ReactNode }) => {
   return (
     <motion.div
       // className={}
-      transition={{ duration: .75, ease: 'linear' }}
+      transition={{ duration: .5, ease: 'linear' }}
       initial={{ opacity: 0 }}
       viewport={{ once: true }}
       whileInView={{
         opacity: 1,
-        scale: [0, .5, 1, 1.25, 1],
-        rotate: [0, 360],
+        scale: [.5, 1],
+        // rotate: [0, 360],
       }}
     >
       {children}
